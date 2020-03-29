c6ce4ab524e1a687ee0fe42624633780c4ccdac8
Joshua Haber <jxh6709@rit.edu>
2020-03-29 12:40:55 -0400

    Domo C Complete

---
+++ server/controllers/Account.js
@@ -0,0 +1,93 @@
+const models = require('../models');
+
+const { Account } = models;
+
+const loginPage = (req, res) => {
+  res.render('login', { csrfToken: req.csrfToken() });
+};
+
+const signupPage = (req, res) => {
+  res.render('signup', { csrfToken: req.csrfToken() });
+};
+
+const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+};
+
+const login = (request, response) => {
+  // ?
+  const req = request;
+  const res = response;
+
+  // casting our roles
+  const username = `${req.body.username}`;
+  const password = `${req.body.pass}`;
+
+  if (!username || !password) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  return Account.AccountModel.authenticate(username, password, (err, account) => {
+    if (err || !account) {
+      return res.status(401).json({ error: 'Wrong username or password' });
+    }
+
+    // B storing data
+    req.session.account = Account.AccountModel.toAPI(account);
+
+    return res.json({ redirect: '/maker' });
+  });
+};
+
+const signup = (request, response) => {
+  // ?
+  const req = request;
+  const res = response;
+
+  // casting for security
+  req.body.username = `${req.body.username}`;
+  req.body.pass = `${req.body.pass}`;
+  req.body.pass2 = `${req.body.pass2}`;
+
+  // validate
+  if (!req.body.username || !req.body.pass || !req.body.pass2) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  if (req.body.pass !== req.body.pass2) {
+    return res.status(400).json({ error: 'Passwords do not match' });
+  }
+
+  return Account.AccountModel.generateHash(req.body.pass, (salt, hash) => {
+    const accountData = {
+      username: req.body.username,
+      salt,
+      password: hash,
+    };
+
+    const newAccount = new Account.AccountModel(accountData);
+
+    const savePromise = newAccount.save();
+
+    savePromise.then(() => {
+      req.session.account = Account.AccountModel.toAPI(newAccount);
+      return res.json({ redirect: '/maker' });
+    });
+
+    savePromise.catch((err) => {
+      if (err.code === 11000) {
+        return res.status(400).json({ error: 'Username is taken' });
+      }
+      return res.status(400).json({ error: 'An error occured' });
+    });
+  });
+};
+
+module.exports = {
+  loginPage,
+  login,
+  logout,
+  signupPage,
+  signup,
+};
